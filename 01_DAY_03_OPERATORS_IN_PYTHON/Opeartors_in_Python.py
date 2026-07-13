f4c4fd557d5271cd31c6f34c59f0e5a5d21c4ae0
Aman Kumar <amankumar00573@gmail.com>
2026-07-13 23:54:30 +0530

    Update variable values and add new operations

--- 01_DAY_03_OPERATORS_IN_PYTHON/Opeartors_in_Python.py
+++ 01_DAY_03_OPERATORS_IN_PYTHON/Opeartors_in_Python.py
@@ -1,10 +1,16 @@
-a = 57
-b = 67
+a = 5
+b = 6
 sum = a+b
 sub = a-b
 multi = a*b
 div = a/b
+modulus = a%b
+exponent = 2**3
+floor_division = a//b
 print("Sum of a + b is :-",sum)
 print("Sub of a + b is :-",sub)
 print("Multi of a*b is :-",multi)
 print("Div of a and b is :-",div)
+print("modulus of a and b is :-",modulus)
+print("Exponent of 2 and 3 is :-",exponent)
+print("Floor division of a nd b is :-",floor_division)
