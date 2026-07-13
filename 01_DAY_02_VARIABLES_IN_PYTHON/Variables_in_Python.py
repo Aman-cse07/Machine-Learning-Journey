4f340bfd1566e23639dee37a93e9c09938a1c799
Aman Kumar <amankumar00573@gmail.com>
2026-07-13 23:22:29 +0530

    Update and rename Variables_in_Python to Variables_in_Python.py

---
+++ 01_DAY_02_VARIABLES_IN_PYTHON/Variables_in_Python.py
@@ -0,0 +1,20 @@
+name="Aman Kumar"
+age = 23
+roll_no = 23707
+marks=89.45
+print(name)
+print(age)
+print(roll_no)
+print(type(name))
+print(type(age))
+print(type(roll_no))
+print("My name is :-", name)
+print("My age is:-", age)
+print("My roll no. is :-", roll_no)
+print(type(marks))
+age2=25
+old=True
+a=None
+print(type(age2))
+print(type(old))
+print(type(a))
