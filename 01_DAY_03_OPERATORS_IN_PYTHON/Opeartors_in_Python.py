b752defe6684daef7a32d7a5fa6fcfef0b07b49d
Aman Kumar <amankumar00573@gmail.com>
2026-07-13 23:44:52 +0530

    Update Opeartors_in_Python.py

--- 01_DAY_03_OPERATORS_IN_PYTHON/Opeartors_in_Python.py
+++ 01_DAY_03_OPERATORS_IN_PYTHON/Opeartors_in_Python.py
@@ -1,1 +1,10 @@
-
+a = 57
+b = 67
+sum = a+b
+sub = a-b
+multi = a*b
+div = a/b
+print("Sum of a + b is :-",sum)
+print("Sub of a + b is :-",sub)
+print("Multi of a*b is :-",multi)
+print("Div of a and b is :-",div)
