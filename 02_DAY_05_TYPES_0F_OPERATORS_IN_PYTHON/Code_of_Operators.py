32323c297ec8a1543553fddd0331714f19e5bca4
Aman Kumar <amankumar00573@gmail.com>
2026-07-19 20:54:20 +0530

    Add examples for various Python operators

--- 02_DAY_05_TYPES_0F_OPERATORS_IN_PYTHON/Code_of_Operators.py
+++ 02_DAY_05_TYPES_0F_OPERATORS_IN_PYTHON/Code_of_Operators.py
@@ -1,1 +1,45 @@
+a = 6 
+b = 7
+c = a + b
+print(c)
 
+#Assignment operators
+a = 4-2
+print(a)
+b = 6
+b += 3 #Increment the value of b by 3 then assign it to b.
+b -= 3 #Decrement the value of b by 3 then assign it to b.
+print(b)
+
+#Comparison operators
+
+d = 5>=5
+print(d)
+
+dd = 5<=5
+print(dd)
+
+c = 7 == 7
+print(c)
+
+e = 5!=5
+print(e)
+
+# Logical Operators
+
+# Truth table of 'or'
+print("True or False is:-", True or False)
+print("True or True is:-", True or True)
+print("False or True is:-", False or True)
+print("False or False is:-", False or False)
+
+#Truth table of 'and'
+print("True and False is:-", True and False)
+print("True and True is:-", True and True)
+print("False and True is:-", False and True)
+print("False and False is:-", False and False)
+
+# 'not'
+
+print(not(True))
+print(not(False))
