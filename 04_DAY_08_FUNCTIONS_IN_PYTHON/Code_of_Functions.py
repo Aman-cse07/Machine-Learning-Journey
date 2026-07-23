157aee2e19106140c41625837e1b8bdcf4122049
Aman Kumar <amankumar00573@gmail.com>
2026-07-23 13:52:25 +0530

    Update Code_of_Functions.py

--- 04_DAY_08_FUNCTIONS_IN_PYTHON/Code_of_Functions.py
+++ 04_DAY_08_FUNCTIONS_IN_PYTHON/Code_of_Functions.py
@@ -1,1 +1,12 @@
+def Bpmce():
+    print("BPMCE , MADHEPURA")
+Bpmce()    
 
+def self():
+    print("Aman Kumar 23157128040 cse(AI&ML)")
+self()
+
+
+def self1():
+    return "Aman Kumar 23707 cse(AI&ML)"
+print(self1())
