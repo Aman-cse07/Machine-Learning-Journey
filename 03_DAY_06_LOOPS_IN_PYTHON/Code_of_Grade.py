e7ba8aaba7effac4977caf5333c77c5d5fe8553f
Aman Kumar <amankumar00573@gmail.com>
2026-07-23 13:35:34 +0530

    Update Code_of_Grade.py

--- 03_DAY_06_LOOPS_IN_PYTHON/Code_of_Grade.py
+++ 03_DAY_06_LOOPS_IN_PYTHON/Code_of_Grade.py
@@ -1,9 +1,12 @@
-marks = int(input("Enter your marks:"))
-if (0<= marks and marks>40):
+marks = int(input("Enter your marks: "))
+
+if 0 <= marks <= 40:
     print("The grade is D")
-elif (40<= marks and marks >60):
+elif 40 < marks <= 60:
     print("The grade is C")
-elif (60<= marks  and marks>80):
+elif 60 < marks <= 80:
     print("The grade is B")
-elif (80 <= marks and marks >=100):
+elif 80 < marks <= 100:
     print("The grade is A")
+else:
+    print("Invalid marks")
