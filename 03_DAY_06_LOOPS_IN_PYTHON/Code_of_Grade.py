0007e762d9de2c31a24c838a83bdb85f3cad4abd
Aman Kumar <amankumar00573@gmail.com>
2026-07-23 13:28:21 +0530

    Fix comparison operators and improve grading logic

--- 03_DAY_06_LOOPS_IN_PYTHON/Code_of_Grade.py
+++ 03_DAY_06_LOOPS_IN_PYTHON/Code_of_Grade.py
@@ -1,1 +1,20 @@
+a,b,c=10,20,30
+if a>b:
+    if a>c:
+        print("a is largest Number")
+else:
+    if b>c:
+        print("b is largest number")
+    else:
+        print("C is largest number")
 
+
+marks = int(input("Enter your marks:"))
+if (0<= marks and marks>40):
+    print("The grade is D")
+elif (40<= marks and marks >60):
+    print("The grade is C")
+elif (60<= marks  and marks>80):
+    print("The grade is B")
+elif (80 <= marks and marks >=100):
+    print("The grade is A")
