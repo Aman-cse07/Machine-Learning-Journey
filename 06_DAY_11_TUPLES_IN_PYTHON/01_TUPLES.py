b72b31c030913533e0c3465c8f5400580a0ef8dc
Aman Kumar <amankumar00573@gmail.com>
2026-08-02 23:40:09 +0530

    Update 01_TUPLES.py

--- 06_DAY_11_TUPLES_IN_PYTHON/01_TUPLES.py
+++ 06_DAY_11_TUPLES_IN_PYTHON/01_TUPLES.py
@@ -1,1 +1,36 @@
+a = (2 , 23 , 23 , 24.78 , "Aman" , "Kumar" , True , "Aman" , "Aman" ,1)
+print(type(a))
 
+i = a.count(23)
+print(i)
+
+j = a.count("Aman")
+print(j)
+
+k = a.index("Aman")
+print(k)
+
+l = a.index(True)
+print(l)
+
+print(len(a))
+
+print("Aman" in a)
+print(False in a)
+print(a[0:3])
+
+b = ( 1 , 2 , 4 , 56 , 65)
+c = (65 , 2 , 23 , 56 )
+
+d = b + c
+print(d)
+
+print(len(d))
+
+print( 2 in b)
+print(3 in b)
+print(c[1:])
+print(b[0:])
+
+a , b , c , d = c
+print(a , b , c , )
