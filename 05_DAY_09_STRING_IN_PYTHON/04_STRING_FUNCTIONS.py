67a5fc051495b21c4591115444902958bd829c20
Aman Kumar <amankumar00573@gmail.com>
2026-08-02 18:51:06 +0530

    Add string functions to demonstrate string methods

--- 05_DAY_09_STRING_IN_PYTHON/04_STRING_FUNCTIONS.py
+++ 05_DAY_09_STRING_IN_PYTHON/04_STRING_FUNCTIONS.py
@@ -1,1 +1,7 @@
-
+name = "Aman Kumar"
+print(len(name))
+print(name.endswith("n Kumar"))
+print(name.startswith("Ama"))
+print(name.count("a"))
+print(name.find("Kumar"))
+print(name.replace("Kumar","Sinha"))
