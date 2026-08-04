83a1c74e311eaf52c9dc06840e9ede45662bc108
Aman Kumar <amankumar00573@gmail.com>
2026-08-04 23:01:55 +0530

    Add dictionary for student marks and retrieval
    Added a dictionary to store names and marks, and implemented user input to retrieve marks.

--- 07_DAY_12_DICTIONARY_IN_PYTHON/01_DICTIONARY.py
+++ 07_DAY_12_DICTIONARY_IN_PYTHON/01_DICTIONARY.py
@@ -1,1 +1,14 @@
+marks = {
+    "Aman":98,
+    "Kumar":87,
+    "Kunal":88,
+    "Piyush":87,
+    "Aiyush":85,
+    "List":[1, 2, 3, 4, 5]
+}
 
+print(marks, type(marks))
+print(marks["Aman"])
+print(marks["List"])
+n = input("Enter name:-") #For users if key match then return value from dictionary otherwise return "None".
+print("Marks of entered name is:- ", marks.get(n))
