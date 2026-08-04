95ff0ffeab6488e68ea40d2c0be450127bc24681
Aman Kumar <amankumar00573@gmail.com>
2026-08-04 23:10:28 +0530

    Add student dictionary and demonstrate methods

--- 07_DAY_12_DICTIONARY_IN_PYTHON/03_DICTIONARY_METHODS.py
+++ 07_DAY_12_DICTIONARY_IN_PYTHON/03_DICTIONARY_METHODS.py
@@ -1,1 +1,20 @@
-
+student = {
+    "Name": "Aman Kumar",
+    "Reg. No.": 23157128040,
+    "Semester": "3rd-Sem",
+    "Branch": "CSE(AI&ML)",
+    "Score":{
+        "OOPs": 19,
+        "DSA": 14,
+        "AEC": 18,
+        "M-III": 16.5,
+        "TW": 14.5
+    }
+}
+print(student.keys())
+print(student.values())
+print(student.items())
+print(list(student.keys()))
+print(tuple(student.keys()))
+print(student["Score"])
+print(student.get("Aman"))
