7899689282d72412bb169f8038cc6795305aaed4
Aman Kumar <amankumar00573@gmail.com>
2026-08-04 23:06:34 +0530

    Update 02_DICTIONARY_METHODS.py

--- 07_DAY_12_DICTIONARY_IN_PYTHON/02_DICTIONARY_METHODS.py
+++ 07_DAY_12_DICTIONARY_IN_PYTHON/02_DICTIONARY_METHODS.py
@@ -1,1 +1,17 @@
-
+marks = {
+    "Name":"Aman Kumar",
+    "Aman":98,
+    "Kumar":87,
+    "Kunal":88,
+    "Piyush":87,
+    "Aiyush":85,
+    1 : "Sinha"
+}
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update({"Aman":100, "name":"Saumya Singh" , "name_1":"Shivangi Rathore"})
+print(marks["Aman"])
+print(marks)
+print(marks.get("sahil")) #This return none value.
+print(marks["sahil"]) #This return error.
