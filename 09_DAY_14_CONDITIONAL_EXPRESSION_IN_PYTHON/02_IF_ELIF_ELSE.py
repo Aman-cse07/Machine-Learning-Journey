5e7cc7fc7d42ea87aa1430ea17ba56bc3321841f
Aman Kumar <amankumar00573@gmail.com>
2026-08-07 00:44:46 +0530

    Add age validation with conditional statements

--- 09_DAY_14_CONDITIONAL_EXPRESSION_IN_PYTHON/02_IF_ELIF_ELSE.py
+++ 09_DAY_14_CONDITIONAL_EXPRESSION_IN_PYTHON/02_IF_ELIF_ELSE.py
@@ -1,1 +1,18 @@
+a = int(input("Enter your age:-"))
 
+if(a>=18):
+    print("You are above the age of consent.")
+    print("Good for you.")
+
+elif(a<0):
+    print("You are entering an invalid negative age.")
+    print("Enter Valid age.")
+
+elif(a==0):
+    print("You are entering zero age i.e Invalid.")
+    print("Enter valid age.")
+
+else:
+    print("You are below the age of consent.")
+
+print("End of Program.")
