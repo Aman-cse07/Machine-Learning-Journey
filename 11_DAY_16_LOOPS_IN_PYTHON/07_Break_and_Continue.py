de9da414dae3f8ccc9ee4565b666769295b97af5
Aman Kumar <amankumar00573@gmail.com>
2026-08-11 23:51:38 +0530

    Update 07_Break_and_Continue.py

--- 11_DAY_16_LOOPS_IN_PYTHON/07_Break_and_Continue.py
+++ 11_DAY_16_LOOPS_IN_PYTHON/07_Break_and_Continue.py
@@ -1,1 +1,26 @@
+for i in range(0,100):
+    print(i)
+    if i == 3:
+        break # break is used for exit the loop right now.
 
+for i in range(100):
+    if (i == 5):
+        break
+    print(i)
+
+for i in range(0,6):
+    if(i == 3):
+        continue
+    print(i)
+
+for i in range(12):
+    print("Printing")
+    if(i == 5):
+        continue
+    print(i)
+        
+
+for i in range(0,8):
+    if(i == 3 or i == 5):
+        continue
+    print(i)
